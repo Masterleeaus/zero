4f1f6c5960e0d5908db5c3619aee4a956dae121b
IOSONOSOI <masterlee.aus@gmail.com>
2026-04-13 23:54:54 +1000

    Delete app directory

--- app/Events/Money/LiquidityRiskDetected.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-namespace App\Events\Money;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
-class LiquidityRiskDetected {
-    use Dispatchable, SerializesModels;
-    public function __construct(public readonly int $companyId, public readonly array $detail) {}
-}
--- app/Extensions/TitanRewind/resources/lang/en/messages.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'cases' => 'Cases',
-    'case' => 'Case',
-    'fix' => 'Fix',
-    'audit_events' => 'Audit Events',
-];
