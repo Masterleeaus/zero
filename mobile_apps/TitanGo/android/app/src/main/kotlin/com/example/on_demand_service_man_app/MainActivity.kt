12a3449df79d2d753ebb524c3346617c2c43a1eb
IOSONOSOI <masterlee.aus@gmail.com>
2026-04-15 16:22:40 +1000

    Delete mobile_apps directory

--- mobile_apps/TitanGo/android/app/src/main/kotlin/com/example/on_demand_service_man_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.sixamtech.demandium.serviceman
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- mobile_apps/TitanPro/lib/feature/service_details/model/variant_model.dart
+++
@@ -1,6 +0,0 @@
-class VariantModel{
-  String variantName;
-  double price;
-
-  VariantModel({required this.variantName,required this.price});
-}
